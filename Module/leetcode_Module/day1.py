632ccdb991a5e3aef88b4dba3dd1353eac0b107a
subinthapa2092 <subinthapa2092@gmail.com>
2026-01-28 12:23:04 +0545

    updated file

---
+++ Module/leetcode_Module/day1.py
@@ -0,0 +1,10 @@
+#### Python in a program to add two list number and revese  it's 
+### Example :: 
+### list1 = [1,2,3] and list2 = [111] now list1+list2 = 123 +111 = [234] then 
+### revese = [432]
+list1 = [1,2,3]
+list2 = [1,1,1]
+result = sum(list1) + sum(list2)
+print(result)
+# empty = result [::-1]
+# print(empty)
